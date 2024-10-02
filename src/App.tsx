c8a199de019c033333e1a0057011948da034e0c0
Jason Korol <ejkorol@gmail.com>
2024-10-02 00:03:12 -0400

    simplify logic, add animations with framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { digitMap, symbols } from "./dots";
 
 interface TimeBlock {
@@ -9,196 +10,73 @@ interface TimeBlock {
 
 function App() {
   const [sec, setSec] = useState<boolean>(false);
-
-  const blink = (col: number): string => {
-    if (col && sec) {
-      return "bg-light";
-    } else if (col && !sec) {
-      return "bg-dark";
-    } else {
-      return "bg-dark";
-    }
-  };
-
   const [timeBlocks, setTimeBlocks] = useState<TimeBlock>({
     hh: [],
     mm: [],
     ss: [],
   });
 
-  const getTime = () => {
+  const blink = (col: number): string => (col && sec ? "bg-light" : "bg-dark");
+
+  const formatTime = () => {
     const d = new Date();
-    const hh = d.getHours();
-    const mm = d.getMinutes();
-    const ss = d.getSeconds();
+    const [hh, mm, ss] = [d.getHours(), d.getMinutes(), d.getSeconds()].map(
+      (unit) => String(unit).padStart(2, "0").split(""),
+    );
 
-    formatTime(hh, mm, ss);
-  };
-
-  const formatTime = (hours: number, minutes: number, seconds: number) => {
-    const hh = String(hours).padStart(2, "0");
-    const mm = String(minutes).padStart(2, "0");
-    const ss = String(seconds).padStart(2, "0");
-
-    const hhs = hh.split("");
-    const mms = mm.split("");
-    const sss = ss.split("");
-
-    const hourBlocks = hhs.map((h) => digitMap.get(Number(h)) || []);
-    const minuteblocks = mms.map((m) => digitMap.get(Number(m)) || []);
-    const secondBlocks = sss.map((s) => digitMap.get(Number(s)) || []);
-
-    setTimeBlocks(() => ({
-      hh: hourBlocks,
-      mm: minuteblocks,
-      ss: secondBlocks,
-    }));
+    setTimeBlocks({
+      hh: hh.map((h) => digitMap.get(Number(h)) || []),
+      mm: mm.map((m) => digitMap.get(Number(m)) || []),
+      ss: ss.map((s) => digitMap.get(Number(s)) || []),
+    });
   };
 
   useEffect(() => {
-    getTime();
+    formatTime();
     const intervalId = setInterval(() => {
-      getTime();
-      setSec((pre) => !pre);
+      formatTime();
+      setSec((prev) => !prev);
     }, 1000);
     return () => clearInterval(intervalId);
   }, []);
 
-  return (
-    <main className="bg-black flex h-svh w-full justify-center items-center">
-      <div className="flex gap-2">
-        <div className="flex flex-col w-fit">
-          {symbols.get("col")!.map((row, idx) => (
-            <div key={idx} className="flex gap-2 mb-2">
-              {row.map((col, idx) => (
-                <div
-                  key={idx}
-                  style={{ height: 12, width: 12, borderRadius: 6 }}
-                  className={col ? "bg-light" : "bg-dark"}
-                />
-              ))}
-            </div>
-          ))}
-        </div>
+  const renderDots = (block: number[][]) =>
+    block.map((row, idx) => (
+      <div key={idx} className="flex gap-2 mb-2">
+        {row.map((col, idx) => (
+          <motion.div
+            key={idx}
+            style={{ height: 12, width: 12, borderRadius: 6 }}
+            className={col ? "bg-light" : "bg-dark"}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: col ? 1 : 0.5 }}
+            transition={{ duration: 0.5 }}
+          />
+        ))}
+      </div>
+    ));
 
+  return (
+    <main className="bg-black flex h-screen w-full justify-center items-center">
+      <div className="flex gap-2">
         <div>
-          <div className="flex flex-col w-fit">
-            {symbols.get("row")!.map((row, idx) => (
-              <div key={idx} className="flex gap-2 mb-2">
-                {row.map((col, idx) => (
-                  <div
-                    key={idx}
-                    style={{ height: 12, width: 12, borderRadius: 6 }}
-                    className={col ? "bg-light" : "bg-dark"}
-                  />
-                ))}
-              </div>
-            ))}
-          </div>
           <div className="flex gap-2 w-fit">
             {timeBlocks.hh.map((block, idx) => (
-              <div key={idx}>
-                {block.map((row, idx) => (
-                  <div key={idx} className="flex gap-2 mb-2">
-                    {row.map((col, idx) => (
-                      <div
-                        key={idx}
-                        style={{ height: 12, width: 12, borderRadius: 6 }}
-                        className={col ? "bg-light" : "bg-dark"}
-                      />
-                    ))}
-                  </div>
-                ))}
-              </div>
+              <div key={idx}>{renderDots(block)}</div>
             ))}
 
-            <div className="flex flex-col">
-              {symbols.get(":")!.map((row, idx) => (
-                <div key={idx} className="flex gap-2 mb-2">
-                  {row.map((col, idx) => (
-                    <div
-                      key={idx}
-                      style={{ height: 12, width: 12, borderRadius: 6 }}
-                      className={blink(col)}
-                    />
-                  ))}
-                </div>
-              ))}
-            </div>
+            <div className="flex flex-col">{renderDots(symbols.get(":")!)}</div>
 
             {timeBlocks.mm.map((block, idx) => (
-              <div key={idx}>
-                {block.map((row, idx) => (
-                  <div key={idx} className="flex gap-2 mb-2">
-                    {row.map((col, idx) => (
-                      <div
-                        key={idx}
-                        style={{ height: 12, width: 12, borderRadius: 6 }}
-                        className={col ? "bg-light" : "bg-dark"}
-                      />
-                    ))}
-                  </div>
-                ))}
-              </div>
+              <div key={idx}>{renderDots(block)}</div>
             ))}
 
-            <div className="flex flex-col">
-              {symbols.get(":")!.map((row, idx) => (
-                <div key={idx} className="flex gap-2 mb-2">
-                  {row.map((col, idx) => (
-                    <div
-                      key={idx}
-                      style={{ height: 12, width: 12, borderRadius: 6 }}
-                      className={blink(col)}
-                    />
-                  ))}
-                </div>
-              ))}
-            </div>
+            <div className="flex flex-col">{renderDots(symbols.get(":")!)}</div>
 
             {timeBlocks.ss.map((block, idx) => (
-              <div key={idx}>
-                {block.map((row, idx) => (
-                  <div key={idx} className="flex gap-2 mb-2">
-                    {row.map((col, idx) => (
-                      <div
-                        key={idx}
-                        style={{ height: 12, width: 12, borderRadius: 6 }}
-                        className={col ? "bg-light" : "bg-dark"}
-                      />
-                    ))}
-                  </div>
-                ))}
-              </div>
+              <div key={idx}>{renderDots(block)}</div>
             ))}
           </div>
-          <div className="flex flex-col w-fit">
-            {symbols.get("row")!.map((row, idx) => (
-              <div key={idx} className="flex gap-2 mb-2">
-                {row.map((col, idx) => (
-                  <div
-                    key={idx}
-                    style={{ height: 12, width: 12, borderRadius: 6 }}
-                    className={col ? "bg-light" : "bg-dark"}
-                  />
-                ))}
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="flex flex-col w-fit">
-          {symbols.get("col")!.map((row, idx) => (
-            <div key={idx} className="flex gap-2 mb-2">
-              {row.map((col, idx) => (
-                <div
-                  key={idx}
-                  style={{ height: 12, width: 12, borderRadius: 6 }}
-                  className={col ? "bg-light" : "bg-dark"}
-                />
-              ))}
-            </div>
-          ))}
         </div>
       </div>
     </main>
